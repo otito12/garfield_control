a6245643226232f12a9dd043df5f49d14d86afd5
Otitodirichukwu <oodarluz@syr.edu>
2023-04-08 23:55:16 -0400

    ik approx functions and marching done

--- garfield/garfield.py
+++ garfield/garfield.py
@@ -4,11 +4,13 @@ import serial.serialutil
 import subprocess
 import time
 import os
+import math
 import playsound
 import speech_recognition as sr
 from gtts import gTTS
 import pyttsx3
 from gcommands import speech_to_command
+from gkinematics import *
 
 ports = serial.tools.list_ports.comports()
 LX16A.initialize(ports[1].device, 0.1)
@@ -21,12 +23,17 @@ LX16A.initialize(ports[1].device, 0.1)
 
 class Garfield():
 
+    # Servos
     l_hip = None
     l_knee = None
     l_calf = None
     r_hip = None
     r_knee = None
     r_calf = None
+
+    # motion
+
+    # Voice control
     wake_phrase = "hey garfield"
 
     def __init__(self):
@@ -36,7 +43,28 @@ class Garfield():
         # load in servos quit program if fails
         # self.speak("Hello world, I am garfield")
         self._load_servos()
-        self.listen()
+        self.move_l_leg(0,0,0)
+        self.move_r_leg(0,0,0)
+        time.sleep(2)
+        for i in range(50):
+            self.move_l_leg(0,0,i)
+            self.move_r_leg(0,0,i)
+            print()
+        time.sleep(2)
+        t = 1
+        while True:
+            if t == 1:
+                self.move_l_leg(0,0,50)
+                self.move_r_leg(0,0,0)
+                t = 0
+            else:
+                self.move_l_leg(0,0,0)
+                self.move_r_leg(0,0,50)
+                t = 1
+            time.sleep(.6)
+        # self.move_r_leg(0,0,0) #should produce origin pose
+        # speech_to_command(self, "move to base origin pose")
+        # self.listen()
 
     def _load_servos(self):
         # ----- init l_hip ----- #
@@ -98,6 +126,45 @@ class Garfield():
         except ServoTimeoutError as e:
             print(f"Servo {e.id_} is not responding. Exiting...")
             quit()
+    
+    # move left leg 
+    # add x,y later
+    # consider base pose 0 and mask offsets
+    def move_l_leg(self,x,y,z,speed=600):
+        # min 76 mm 
+        # hip_offest = 0
+        knee_offset = 26.6
+        calf_offset = 35.1
+        self.l_hip.move(50,speed)
+        
+        # Calc Z
+        knee_angle = knee_angle_z_ik(z+76) + knee_offset
+        calf_angle = calf_angle_z_ik(z+76)+ calf_offset
+        
+        self.l_knee.move(knee_angle,speed)
+        self.l_calf.move(calf_angle,speed)
+
+        # Adjust for X 
+
+
+        print("l_k:",knee_angle_z_ik(z+76)+ knee_offset)
+        print("l_c:",calf_angle_z_ik(z+76)+ calf_offset)
+
+    # move right leg add x,y later
+    def move_r_leg(self,x,y,z,speed=600):
+        # min 76 mm 
+        # hip_offest = 0
+        knee_offset = -33.4
+        calf_offset = 151
+        self.r_hip.move(37,speed)
+        # becuase the servo is physically flipped but the IK remains the same
+        knee_base = knee_angle_z_ik(76)+ knee_offset
+        calf_base = calf_angle_z_ik(76)+ calf_offset
+        self.r_knee.move(knee_base + (knee_base - (knee_angle_z_ik(z+76)+ knee_offset)),speed)
+        self.r_calf.move(calf_base + (calf_base - (calf_angle_z_ik(z+76)+ calf_offset)),speed)
+        print("r_k:",knee_base + (knee_base - (knee_angle_z_ik(z+76)+ knee_offset)))
+        print("r_c:",calf_base + (calf_base - (calf_angle_z_ik(z+76)+ calf_offset)))
+
 
     def print_physical_angles(self): # debugging
         print("l_hip:", self.l_hip.get_physical_angle(),
@@ -137,6 +204,9 @@ class Garfield():
                 self.speak("You may speak")
                 command = self._get_audio()
                 speech_to_command(self, command)
+
+    
+
             
             
 
--- garfield/gcontrol.py
+++ garfield/gcontrol.py
@@ -4,6 +4,29 @@ import serial.tools.list_ports
 import serial.serialutil
 from garfield import Garfield
 import time
-
 garfiled = Garfield()
 
+#for developing the IK Delete later
+count = 0
+# def knee_angle_fit_z(x):
+#     # approximate Knee pose formular from z axis ik
+#     return -8.102e-05*pow(x,3) + 0.03254*pow(x,2) - 4.848*x + 250.5
+
+# def calf_angle_fit_z(x):
+#     # approximate Knee pose formular from z axis ik
+#     return -1.736e-05*pow(x,3) + 0.006582*pow(x,2) - 1.417*x + 102.2
+
+# # time.sleep(2)
+# # offset = 40.8
+# # l_c_angle = offset +  calf_angle_fit_z(150)
+# # l_k_angle = offset +  knee_angle_fit_z(150)
+# # garfiled.l_calf.move(l_c_angle,2000)
+# # garfiled.l_knee.move(l_k_angle,2000)
+
+# # # Right leg
+# # offset_c = 154.8
+# # offset_k = -20
+# # r_c_angle = offset_c +  calf_angle_fit_z(150)
+# # r_k_angle = offset_k +  knee_angle_fit_z(150)
+# # garfiled.r_calf.move(r_c_angle,2000)
+# # garfiled.r_knee.move(r_k_angle,2000)
